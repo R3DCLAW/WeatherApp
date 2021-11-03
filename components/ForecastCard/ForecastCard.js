c0d4bdb8e4caaef4d77dc0212828ea03b04b6f43
ruben <rubenfrias10@hotmail.com>
2021-11-02 20:35:29 -0400

    First commit

---
+++ components/ForecastCard/ForecastCard.js
@@ -0,0 +1,10 @@
+const ForecastCard = () => {
+  return (
+    <div className="text-white bg-red-500 px-3">
+      <h1>Tomorrow</h1>
+      <img src="/assets/Shower.png" alt=""></img>
+    </div>
+  );
+};
+
+export default ForecastCard;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,68 @@
-import Head from 'next/head'
+import Head from "next/head";
+import ForecastCard from "../components/ForecastCard/ForecastCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="min-h-screen bg-[#1E213A]">
       <Head>
-        <title>Create Next App</title>
+        <title>Weather</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="flex flex-col">
+        {
+          // Left / Top side of the page
+        }
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <div className="relative flex flex-col min-h-screen items-center justify-between pb-32 pt-3">
+          <div className="absolute top-44 overflow-hidden w-6/6">
+            <img
+              className="opacity-10 min-w-full"
+              src="/assets/Cloud-background.png"
+              alt="Cloud background"
+            />
+          </div>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+          <div className="text-white">
+            <button type="button">Search for places</button>
+            <button type="button">Loc</button>
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <div className="">
+            <img
+              className="w-25"
+              src="/assets/Shower.png"
+              alt="Current weather"
+            />
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+          <div className="text-white text-center">
+            <h1 className="text-8xl">
+              15<span className="text-5xl">&#8451;</span>
+            </h1>
+            <p className="text-3xl pt-7">Shower</p>
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="text-white text-center grid grid-cols-2 gap-4">
+            <p>Today</p>
+            <p>Fri, 5 Jun</p>
+            <p className="col-span-2">Helsinki</p>
+          </div>
+        </div>
+
+        {
+          // Right / bottom side of the page
+        }
+
+        <div>
+          <div className="grid grid-cols-2">
+            <ForecastCard />
+            <ForecastCard />
+          </div>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <footer className=""></footer>
     </div>
-  )
+  );
 }
